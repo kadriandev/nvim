c09151506d279c1b54b1e5d3d3dafbfd581a4d97
KyleMonteiro <kmonteir@ALPQ4YVR7GWJ.local>
2023-11-10 17:21:48 -0500

    Create git submodule for nvim

---
+++ after/plugin/ui/colourscheme.lua
@@ -0,0 +1,30 @@
+require("catppuccin").setup({
+	flavour = "mocha",
+	term_colors = true,
+	transparent_background = true,
+	color_overrides = {
+		mocha = {
+			base = "#000000",
+			mantle = "#000000",
+			crust = "#000000",
+		},
+	},
+	integrations = {
+		alpha = true,
+		nvimtree = true,
+		cmp = true,
+		mason = true,
+		harpoon = true,
+		telescope = true,
+		lsp_trouble = true,
+		which_key = true,
+		treesitter = true,
+		indent_blankline = {
+			enabled = true,
+			scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+			colored_indent_levels = false,
+		},
+	},
+})
+
+vim.cmd("colorscheme catppuccin")
---
+++ after/plugin/ui/ui.lua
@@ -0,0 +1,19 @@
+require("dressing").setup({
+	input = {
+		get_config = function()
+			if vim.api.nvim_buf_get_option(0, "filetype") == "NvimTree" then
+				return { enabled = false }
+			end
+		end,
+	},
+})
+
+vim.opt.list = true
+
+require("lualine").setup({
+	options = {
+		theme = "catppuccin",
+	},
+})
+
+require("indent_blankline").setup({})
