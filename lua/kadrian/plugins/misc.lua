816bdb961a35e57f8309de67c8f7b824c390f998
KyleMonteiro <kmonteir@ALPQ4YVR7GWJ.local>
2023-12-19 10:40:47 -0500

    updated plugins

--- lua/kadrian/plugins/misc.lua
+++ lua/kadrian/plugins/misc.lua
@@ -37,4 +37,22 @@ return {
 			require("nvim-ts-autotag").setup()
 		end,
 	},
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				no_wrap = true,
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+		lazy = false,
+	},
 }
