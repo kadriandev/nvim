6f85d0477648794898f30bd48beccb7ac31701d8
kadriandev <kyle.monteiro@gmail.com>
2024-05-03 11:56:47 -0400

    Swapped out fugitive for neogit

--- lua/kadrian/plugins/git.lua
+++ lua/kadrian/plugins/git.lua
@@ -1,11 +1,12 @@
 return {
 	{
-		"tpope/vim-fugitive",
-		config = function()
-			Keymap("n", "<leader>gg", "<cmd>G<cr>", { desc = "Open Git" })
-			Keymap("n", "<leader>gs", "<cmd>G status<cr>", { desc = "Status" })
-			Keymap("n", "<leader>gp", "<cmd>G push<cr>", { desc = "Push" })
-		end,
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
 	},
 	{
 		"FabijanZulj/blame.nvim",
@@ -20,11 +21,35 @@ return {
 			require("gitsigns").setup()
 		end,
 	},
-	{
-		"sindrets/diffview.nvim",
-		config = function()
-			Keymap("n", "<leader>gdo", "<cmd>DiffviewOpen<cr>", { desc = "Open" })
-			Keymap("n", "<leader>gdc", "<cmd>DiffviewClose<cr>", { desc = "Close" })
-		end,
-	},
 }
+
+-- return {
+-- 	{
+-- 		"tpope/vim-fugitive",
+-- 		config = function()
+-- 			Keymap("n", "<leader>gg", "<cmd>G<cr>", { desc = "Open Git" })
+-- 			Keymap("n", "<leader>gs", "<cmd>G status<cr>", { desc = "Status" })
+-- 			Keymap("n", "<leader>gp", "<cmd>G push<cr>", { desc = "Push" })
+-- 		end,
+-- 	},
+-- 	{
+-- 		"FabijanZulj/blame.nvim",
+-- 		config = function()
+-- 			require("blame").setup()
+-- 			Keymap("n", "<leader>gb", "<cmd>BlameToggle<cr>", { desc = "Toggle Git Blame" })
+-- 		end,
+-- 	},
+-- 	{
+-- 		"lewis6991/gitsigns.nvim",
+-- 		config = function()
+-- 			require("gitsigns").setup()
+-- 		end,
+-- 	},
+-- 	{
+-- 		"sindrets/diffview.nvim",
+-- 		config = function()
+-- 			Keymap("n", "<leader>gdo", "<cmd>DiffviewOpen<cr>", { desc = "Open" })
+-- 			Keymap("n", "<leader>gdc", "<cmd>DiffviewClose<cr>", { desc = "Close" })
+-- 		end,
+-- 	},
+-- }
